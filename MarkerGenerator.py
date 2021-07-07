a81e9718d0a39f24483a829ea7dec1ffe5f4bde7
harsh-mandalia <60979698+harsh-mandalia@users.noreply.github.com>
2021-07-07 15:37:14 +0530

    Update and rename aruco.py to MarkerGenerator.py

--- MarkerGenerator.py
+++ MarkerGenerator.py
@@ -1,7 +1,9 @@
 import cv2 as cv
-# cv.drawMarker()
+
 dict=cv.aruco.getPredefinedDictionary(cv.aruco.DICT_6X6_250)
-img=cv.aruco.drawMarker(dict, 2, 200)
+MarkerID=2  #from 0 to 249
+size=200    #200x200 pixels
+img=cv.aruco.drawMarker(dict, MarkerID, size)
 cv.imshow("Generated marker",img)
 cv.imwrite("marker2.png",img)
 cv.waitKey(5000)
