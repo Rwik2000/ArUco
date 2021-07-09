6a570c5690cfdf7f82537338f58d04e9c6766143
harsh-mandalia <60979698+harsh-mandalia@users.noreply.github.com>
2021-07-09 23:29:29 +0530

    added 3 axis and calibration

--- VideoDetection.py
+++ VideoDetection.py
@@ -1,22 +1,36 @@
-import cv2
+from math import dist
+import cv2 as cv
+from numpy.core.numeric import tensordot
+import numpy as np
+
+cameraMatrix=np.array([[785.9682179, 0, 357.16177299], [0,792.81486091, 105.86987584], [0, 0, 1]])
+distCoeffs=np.array([[0.11186678, 0.56365852, -0.06475133, 0.01037753, -1.38199825]])
 
 
-cv2.namedWindow("video")
-cap=cv2.VideoCapture(0)
-dict=cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+cv.namedWindow("video")
+cap=cv.VideoCapture(0)
+dict=cv.aruco.getPredefinedDictionary(cv.aruco.DICT_6X6_250)
 while(cap.isOpened()):
     ret, frame=cap.read()
 
     outputFrame = frame.copy()
-    corners, ids, rejectedImgPoints	= cv2.aruco.detectMarkers(frame, dict)
-    outputFrame = cv2.aruco.drawDetectedMarkers(outputFrame,corners,ids)
-
+    corners, ids, rejectedImgPoints = cv.aruco.detectMarkers(frame, dict)
+    
     if len(corners)>0:
+        outputFrame = cv.aruco.drawDetectedMarkers(outputFrame,corners,ids)
+        rvecs, tvecs, _objPoints = cv.aruco.estimatePoseSingleMarkers(corners, 0.05, cameraMatrix, distCoeffs)
+        for i in range(len(rvecs)):
+            rvec=rvecs[i]
+            tvec=tvecs[i]
+            outputFrame=cv.aruco.drawAxis(outputFrame,cameraMatrix,distCoeffs,rvec,tvec,0.1)
+
         print(corners[0][0][0])         #position (x,y) in terms of pixels
 
-    cv2.imshow("video",outputFrame)
-    if(cv2.waitKey(1)==ord('q')):
+    # outputFrame = cv.aruco.drawAxis(outputFrame, cameraMatrix, distCoeffs, rvec, tvec, 0.1)
+    cv.imshow("video",outputFrame)
+
+    if(cv.waitKey(1)==ord('q')):
         print(outputFrame.shape)        #size of your video in pixels
         break
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
