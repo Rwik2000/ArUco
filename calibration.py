d227841dd8c67176eaa26f910c3001062d354128
harsh-mandalia <60979698+harsh-mandalia@users.noreply.github.com>
2021-07-10 00:01:44 +0530

    Update calibration.py

--- calibration.py
+++ calibration.py
@@ -3,9 +3,11 @@ import cv2 as cv
 
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
 objp = np.zeros((6*7,3), np.float32)
 objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
@@ -16,10 +18,12 @@ cap=cv.VideoCapture(0)
 while(cap.isOpened()):
     ret, img=cap.read()
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    cv.imshow("video",gray)
+    
     # Find the chess board corners
     ret, corners = cv.findChessboardCorners(gray, (7,6), None)
+    
     # If found, add object points, image points (after refining them)
-    cv.imshow("video",gray)
     if ret == True:
         objpoints.append(objp)
         corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
@@ -48,7 +52,3 @@ print("Camera matrix : \n")
 print(mtx)
 print("dist : \n")
 print(dist)
-print("rvecs : \n")
-print(rvecs)
-print("tvecs : \n")
-print(tvecs)
